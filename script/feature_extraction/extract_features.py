6658190bbd2a91d4967d48405a5311d6e8381ba1
Yesid C <yesidcanoc@gmail.com>
2021-10-10 22:00:55 +0200

    Added the feature contain_pthotos
    Returns 1 if the post contains a photo; 0 otherwise

--- script/feature_extraction/extract_features.py
+++ script/feature_extraction/extract_features.py
@@ -14,7 +14,8 @@ import numpy as np
 from script.feature_extraction.character_length import CharacterLength
 from script.feature_extraction.feature_collector import FeatureCollector
 from script.feature_extraction.month_tweet import extract_month
-from script.util import COLUMN_TWEET, COLUMN_LABEL,COLUMN_DATE
+from script.feature_extraction.contain_photo import contain_pthotos
+from script.util import COLUMN_TWEET, COLUMN_LABEL,COLUMN_DATE, COLUMN_PHOTOS
 
 
 # setting up CLI
@@ -25,13 +26,15 @@ parser.add_argument("-e", "--export_file", help = "create a pipeline and export 
 parser.add_argument("-i", "--import_file", help = "import an existing pipeline from the given location", default = None)
 parser.add_argument("-c", "--char_length", action = "store_true", help = "compute the number of characters in the tweet")
 parser.add_argument("-m", "--month_tweet", action= "store_true", help= "retrieve the month the tweet was posted")
+parser.add_argument("-p", "--contain_photo", action= "store_true", help= "returns 1 if the post contains a photo; 0 otherwise")
 args = parser.parse_args()
 
 # load data
 df = pd.read_csv(args.input_file, quoting = csv.QUOTE_NONNUMERIC, lineterminator = "\n")
-#TODO delete this line of code
+
 #TODO change readme reference to code --> change it to script
-df = df.iloc[0:10]
+#TODO delete this line of code
+df = df.iloc[0:5]
 
 if args.import_file is not None:
     # simply import an exisiting FeatureCollector
@@ -46,9 +49,17 @@ else:    # need to create FeatureCollector manually
         # character length of original tweet (without any changes)
         features.append(CharacterLength(COLUMN_TWEET))
     if args.month_tweet:
+        #month tweet was posted
         features.append(extract_month(COLUMN_DATE))
+    if args.contain_photo:
+        #whether photo was included in the tweet
+        features.append(contain_pthotos(COLUMN_PHOTOS))
+
+
     # create overall FeatureCollector
     feature_collector = FeatureCollector(features)
+
+
     
     # fit it on the given data set (assumed to be training data)
     feature_collector.fit(df)
