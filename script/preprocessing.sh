514aed205fc47cd4a4e7d454fa1e9a39be4899b1
Yesid C <yesidcanoc@gmail.com>
2021-10-12 22:46:44 +0200

    Changed references from code to script in all sh files

--- script/preprocessing.sh
+++ script/preprocessing.sh
@@ -5,12 +5,12 @@ mkdir -p data/preprocessing/split/
 
 # add labels
 echo "  creating labels"
-python -m code.preprocessing.create_labels data/raw/ data/preprocessing/labeled.csv
+python -m script.preprocessing.create_labels data/raw/ data/preprocessing/labeled.csv
 
 # other preprocessing (removing punctuation etc.)
 echo "  general preprocessing"
-python -m code.preprocessing.run_preprocessing data/preprocessing/labeled.csv data/preprocessing/preprocessed.csv --punctuation -e data/preprocessing/pipeline.pickle
+python -m script.preprocessing.run_preprocessing data/preprocessing/labeled.csv data/preprocessing/preprocessed.csv --punctuation -e data/preprocessing/pipeline.pickle
 
 # split the data set
 echo "  splitting the data set"
-python -m code.preprocessing.split_data data/preprocessing/preprocessed.csv data/preprocessing/split/ -s 42
+python -m script.preprocessing.split_data data/preprocessing/preprocessed.csv data/preprocessing/split/ -s 42
