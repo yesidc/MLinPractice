3b41b63258edf6adad3cfeb15a8490c3be8cbb09
Argha <argha.sarker.93@gmail.com>
2021-10-17 23:36:25 +0200

    tokenizer fixed

--- script/preprocessing/run_preprocessing.py
+++ script/preprocessing/run_preprocessing.py
@@ -11,7 +11,9 @@ Created on Tue Sep 28 16:43:18 2021
 import argparse, csv, pickle
 import pandas as pd
 from sklearn.pipeline import make_pipeline
+from script.util import COLUMN_TWEET,SUFFIX_TOKENIZED
 from script.preprocessing.punctuation_remover import PunctuationRemover
+from script.preprocessing.tokenizer import Tokenizer
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Various preprocessing steps")
@@ -19,6 +21,11 @@ parser.add_argument("input_file", help = "path to the input csv file")
 parser.add_argument("output_file", help = "path to the output csv file")
 parser.add_argument("-p", "--punctuation", action = "store_true", help = "remove punctuation")
 parser.add_argument("-e", "--export_file", help = "create a pipeline and export to the given location", default = None)
+# tokenizer initianization 
+
+parser.add_argument("-t", "--tokenize", action = "store_true", help = "Tokenize the tweets into words")
+parser.add_argument("--tokenize_input", help = "input column to tokenize", default = COLUMN_TWEET)
+
 args = parser.parse_args()
 
 # load data
@@ -28,7 +35,8 @@ df = pd.read_csv(args.input_file, quoting = csv.QUOTE_NONNUMERIC, lineterminator
 preprocessors = []
 if args.punctuation:
     preprocessors.append(PunctuationRemover())
-
+if args.tokenize: 
+    preprocessors.append(Tokenizer(args.tokenize_input, args.tokenize_input + SUFFIX_TOKENIZED))
 # call all preprocessing steps
 for preprocessor in preprocessors:
     df = preprocessor.fit_transform(df)
