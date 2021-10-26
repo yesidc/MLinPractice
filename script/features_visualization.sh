6529e206f75eae6f664c9946dbfc71ccaefc2b54
John J. Madrid <johnmadridcarvajal@gmail.com>
2021-10-26 14:00:33 +0200

    Creating  .sh and .py file f or visualizations

---
+++ script/features_visualization.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# create directory if not yet existing
+mkdir -p data/features_visualization/
+
+# add labels
+echo "  creating images for features visualization"
+#python -m script.preprocessing.create_labels data/raw/ data/preprocessing/labeled.csv
