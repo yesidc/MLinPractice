6795988cb1c8ff4ea72b310a0361cd0ca3b3132e
Yesid C <yesidcanoc@gmail.com>
2021-11-07 15:19:16 +0100

    CLI feature_hour
    Added parser lines for the new features

--- script/feature_extraction/feature_hour.py
+++ script/feature_extraction/feature_hour.py
@@ -1,3 +1,10 @@
+"""
+Created on Sunday Nov 7  2021
+
+@author: yesidc
+"""
+
+
 import pandas as pd
 from script.feature_extraction.feature_extractor import FeatureExtractor
 
@@ -10,7 +17,7 @@ class extract_hour (FeatureExtractor):
 
 
     def _get_values (self,inputs):
-        """Given the date column, extracts the hour the tweet was posted"""
+        """Given the time column, extracts the hour the tweet was posted"""
         result = pd.DatetimeIndex(inputs[0]).hour.values
         result = result.reshape(-1, 1)
         return result
